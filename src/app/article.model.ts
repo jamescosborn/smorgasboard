36c2e19575919c0e35cd3fb8bde59d3d897b58d2
Joseph Tomlinson <bizzclaw1@gmail.com>
2017-12-18 13:50:02 -0800

    add firebase article service

--- src/app/article.model.ts
+++ src/app/article.model.ts
@@ -1,16 +1,19 @@
+import { ArticleService } from './article-service'
+
 export class Article  {
   id: number;
   data: object;
 
   static getAll() {
-    return []
+    return ArticleService.getArticles();
   }
 
   constructor(data) {
     this.data = data;
   }
+
   save() {
     this.id = Article.getAll().length;
-
+    ArticleService.addArticle(this);
   }
 }
---
+++ src/app/article.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Article } from './article.model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+@Injectable()
+export class ArticleService {
+  articles: FirebaseListObservable<any[]>;
+
+  constructor(private database: AngularFireDatabase) {
+    this.articles = database.list('articles');
+  }
+
+  getArticles() {
+    return this.articles;
+  }
+
+  addArticle(newArticle: Article) {
+    this.articles.push(newArticle);
+  }
+
+  getArticleById(articleId: string) {
+    return this.database.object('articles/' + articleId);
+  }
+
+  updateArticle(localUpdatedArticle) {
+    const articleEntryInFirebase = this.getArticleById(localUpdatedArticle.$key);
+    articleEntryInFirebase.update({
+      title: localUpdatedArticle.title,
+      artist: localUpdatedArticle.artist,
+      description: localUpdatedArticle.description
+    });
+  }
+
+  deleteArticle(localArticleToDelete) {
+    const articleEntryInFirebase = this.getArticleById(localArticleToDelete.$key);
+    articleEntryInFirebase.remove();
+  }
+
+
+}
